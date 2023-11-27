dac169fe24e24097cf26bd850827bd05e15eb44e
Abendiix <abendiix@gmail.com>
2023-11-27 22:23:08 +0100

    Fixing files

--- assets/js/test.js
+++
@@ -1,6 +0,0 @@
-function randomtext() {
-    var randomtxt = ['ʕっ•ᴥ•ʔっ', '※\(^o^)/※', '(◕ᴥ◕ʋ)', '(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧', '(´・ω・)っ由','ଘ(੭*ˊᵕˋ)੭* ̀ˋ ɪɴᴛᴇʀɴᴇᴛ', '( ͡° ͜ʖ ͡°)', '(=^･ｪ･^=))ﾉ彡☆'];
-    return randomtxt[Math.floor((Math.random() * 7.99))];
-}
-
-document.getElementById("spam").innerHTML = randomtext();
