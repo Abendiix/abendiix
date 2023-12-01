2264ecbde508be2090a4064ebf8c29e3dc60c953
Abendiix <abendiix@gmail.com>
2023-12-01 01:47:25 +0100

    Change languageChanged.js to also change the alt texts. Spanish and Japanese will have placeholder by now. Also removed the alt from 'emoti' since it seems that they don't work with <p>

--- assets/js/languageChanged.js
+++ assets/js/languageChanged.js
@@ -44,6 +44,7 @@ function updateTexts(language)
 {
     if(localStorage.getItem("language") == "es")
     {
+        //Texts
         document.getElementById("moto").innerHTML = "¡Juegos y herramientas!";
         document.getElementById("es").style.textDecoration = "underline";
         document.getElementById("en").style.textDecoration = "none";
@@ -54,9 +55,20 @@ function updateTexts(language)
         document.getElementById("openApp").innerHTML = "Abrir aplicación";
         document.getElementById("openApp").href = "mailto:contacto@abendiix.com";
         document.getElementById("emotiText").innerHTML = "¡Me encontraste!";
+
+        //Alt texts
+        document.getElementById("banner").alt = "Placeholder";
+        document.getElementById("spritesloveicon").alt = "Placeholder";
+        document.getElementById("spriteslovelinkicon").alt = "Placeholder";
+        document.getElementById("ko-fi").alt = "Placeholder";
+        document.getElementById("github").alt = "Placeholder";
+        document.getElementById("itchio").alt = "Placeholder";
+        document.getElementById("twitter").alt = "Placeholder";
+
     }
     else if (localStorage.getItem("language") == "en")
     {
+        //Texts
         document.getElementById("moto").innerHTML = "Games and tools!";
         document.getElementById("es").style.textDecoration = "none";
         document.getElementById("en").style.textDecoration = "underline";
@@ -67,9 +79,19 @@ function updateTexts(language)
         document.getElementById("openApp").innerHTML = "Open app";
         document.getElementById("openApp").href = "mailto:contact@abendiix.com";
         document.getElementById("emotiText").innerHTML = "You found me!";
+
+        //Alt texts
+        document.getElementById("banner").alt = "Abendiix's logo expanded in banner form. The letters are black and the background of the image is transparent.";
+        document.getElementById("spritesloveicon").alt = "Logo/Icon for the project called 'Sprites Love'. It shows a pixelated S from the word 'Sprites' and one pixel down and to the right there's a red pixelated heart representing the word 'Love'.";
+        document.getElementById("spriteslovelinkicon").alt = "Black icon that symbolizes a website link with two diagonal chains that is also used a link to visit the website of the project 'Sprites Love'.";
+        document.getElementById("ko-fi").alt = "Black icon of the platform Ko-fi, used to redirect to Abendiix's profile.";
+        document.getElementById("github").alt = "Black icon of the platform GitHub, used to redirect to Abendiix's profile.";
+        document.getElementById("itchio").alt = "Black icon of the platform itch.io, used to redirect to Abendiix's profile.";
+        document.getElementById("twitter").alt = "Black icon of the platform Twitter, used to redirect to Abendiix's profile.";
     }
     else if (localStorage.getItem("language") == "ja")
     {
+        //Texts
         document.getElementById("moto").innerHTML = "ゲームとツール！";
         document.getElementById("es").style.textDecoration = "none";
         document.getElementById("en").style.textDecoration = "none";
@@ -80,5 +102,14 @@ function updateTexts(language)
         document.getElementById("openApp").innerHTML = "アプリを開く";
         document.getElementById("openApp").href = "mailto:contact@abendiix.com";
         document.getElementById("emotiText").innerHTML = "私を見つけた！";
+
+        //Alt texts
+        document.getElementById("banner").alt = "Placeholder";
+        document.getElementById("spritesloveicon").alt = "Placeholder";
+        document.getElementById("spriteslovelinkicon").alt = "Placeholder";
+        document.getElementById("ko-fi").alt = "Placeholder";
+        document.getElementById("github").alt = "Placeholder";
+        document.getElementById("itchio").alt = "Placeholder";
+        document.getElementById("twitter").alt = "Placeholder";
     }
 }
