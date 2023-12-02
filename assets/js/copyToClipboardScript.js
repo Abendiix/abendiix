e6bdf6226ea138ccfa01b92a88b61104e78ac2b7
Abendiix <abendiix@gmail.com>
2023-12-02 22:04:31 +0100

    Made some accessibility changes such as changing the header to an h1 with a link, the size of headers across the page and changing the copy button/link with a timed function from https://stackoverflow.com/questions/14226803/wait-5-seconds-before-executing-next-line

--- assets/js/copyToClipboardScript.js
+++ assets/js/copyToClipboardScript.js
@@ -8,4 +8,21 @@ function copyEmail()
     {
         navigator.clipboard.writeText("contact@abendiix.com");
     }
+
+    document.getElementById("copy").style.textDecoration = "none";
+    document.getElementById("copy").innerHTML = "Copied!";
+
+    //Improve responsiveness and accessibility for the copy email button
+    delay(function(){
+        document.getElementById("copy").innerHTML = "Copy";
+        document.getElementById("copy").style.textDecoration = "underline";
+    }, 1200 );
 }
+
+var delay = (function() {
+    var timer = 0;
+    return function(callback, ms) {
+        clearTimeout (timer);
+        timer = setTimeout(callback, ms);
+    };
+})();
