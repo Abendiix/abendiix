39ecca3a7f2236a215f36d2f48d31c9cc3b0a513
Abendiix <abendiix@gmail.com>
2023-12-03 19:35:36 +0100

    Adapt the copy email text for spanish and japanese

--- assets/js/copyToClipboardScript.js
+++ assets/js/copyToClipboardScript.js
@@ -10,13 +10,38 @@ function copyEmail()
     }
 
     document.getElementById("copy").style.textDecoration = "none";
-    document.getElementById("copy").innerHTML = "Copied!";
 
-    //Improve responsiveness and accessibility for the copy email button
-    delay(function(){
-        document.getElementById("copy").innerHTML = "Copy";
-        document.getElementById("copy").style.textDecoration = "underline";
-    }, 1200 );
+
+    if(localStorage.getItem("language") == "es")
+    {
+        document.getElementById("copy").innerHTML = "¡Copiado!";
+
+        //Improve responsiveness and accessibility for the copy email button
+        delay(function(){
+            document.getElementById("copy").innerHTML = "Copiar";
+            document.getElementById("copy").style.textDecoration = "underline";
+        }, 1200 );
+    }
+    else if (localStorage.getItem("language") == "en")
+    {
+        document.getElementById("copy").innerHTML = "Copied!";
+
+        //Improve responsiveness and accessibility for the copy email button
+        delay(function(){
+            document.getElementById("copy").innerHTML = "Copy";
+            document.getElementById("copy").style.textDecoration = "underline";
+        }, 1200 );
+    }
+    else if (localStorage.getItem("language") == "ja")
+    {
+        document.getElementById("copy").innerHTML = "コピー済み！";
+
+        //Improve responsiveness and accessibility for the copy email button
+        delay(function(){
+            document.getElementById("copy").innerHTML = "コピー";
+            document.getElementById("copy").style.textDecoration = "underline";
+        }, 1200 );
+    }
 }
 
 var delay = (function() {
